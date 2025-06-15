8909040d7b2e064608d407f5cb22bf6e216222e5
Yash Kumar Rana <2200520100072@ietlucknow.ac.in>
2025-06-15 18:33:06 +0530

    clerkwebhook

--- server/controllers/clerkWebHooks.js
+++ server/controllers/clerkWebHooks.js
@@ -12,15 +12,17 @@ const clerkWebHooks = async (req, res) => {
             "svix-signature": req.headers["svix-signature"],
         };
 
-        // verify returns the actual payload
-        const payload = whook.verify(JSON.stringify(req.body), headers);
+        const payload = await whook.verify(JSON.stringify(req.body), headers);
 
-        const { data, type } = req.body;
+        // @ts-ignore
+        const { data, type } = payload;
+
         const userData = {
             _id: data.id,
             email: data.email_addresses[0].email_address,
             username: `${data.first_name} ${data.last_name}`,
             image: data.image_url,
+            recentSearchedCities: []
         };
 
         switch (type) {
